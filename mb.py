2c6148105a2e87a6dcbd9ec7f826d057dcb2ab45
Mukesh Dalal <mukdal.wt@gmail.com>
2024-02-28 17:12:21 -0500

    multi-arity function for MB

--- mb.py
+++ mb.py
@@ -19,47 +19,48 @@ class ModelBox(): # main MB class
         self.edata_fraction = 0.3 # fraction of cached data randomly chosen for evaluation
         self.feedback_fraction = 0.1 # fraction of calls randomly chosen for feedback from users
 
-    def __call__(self, x):
-        fsum = sum([f(x) for f in self.functions])
-        msum = sum([model.predict([[x]]) for idx, model in enumerate(self.models) if self.isproduction[idx]])
+    def __call__(self, *xs):
+        fsum = sum([f(*xs) for f in self.functions])
+        msum = sum([model.predict([list(xs)]) for idx, model in enumerate(self.models) if self.isproduction[idx]])
         return (msum + fsum) / (sum(self.isproduction) + len(self.functions))
         
     def function_wrapper(self): # wrap MB  around some function f
         def decorator(f):
-            def wrapper(x):
+            def wrapper(*xs):
                 self.host = f
                 if self.state == 'connected':
-                    y = f(x)
-                    y = self.get_feedback(x, y)
+                    y = f(*xs)
+                    y = self.get_feedback(y, *xs)
                 elif self.state == 'production':
-                    y = (self(x) + f(x))[0] / 2
-                    y = self.get_feedback(x, y)
+                    y = (self(*xs) + f(*xs))[0] / 2
+                    y = self.get_feedback(y, *xs)
                 else: # self.state == 'embedded'
-                    y = self(x)[0]
-                self.add_data(x, y)
+                    y = self(*xs)[0]
+                self.add_data(y, *xs)
                 return y
             return wrapper
         return decorator
     
     def sensor_wrapper(self, kind='direct'): # wrap a sensor around some function g
         def decorator(g): # add other sensors as needed
-            def direct(x): # direct sensor
-                y = g(x)
-                self.add_data(x, y)
+            def direct(*xs): # direct sensor
+                y = g(*xs)
+                self.add_data(y, *xs)
                 return y
-            def inverse(y): # inverse sensor
-                x = g(y)
-                self.add_data(x, y)
-                return y
+            def inverse(*yxs): # inverse sensor, yxs = (y, xs except xlast)
+                xlast = g(*yxs)
+                self.add_data(yxs[0], *yxs[0:], xlast)
+                return xlast
             return eval(kind)
+        assert kind in ['direct', 'inverse'], "sensor kind must be 'direct' or 'inverse'"
         return decorator
     
-    def add_data(self, x, y):
+    def add_data(self, y, *xs):
         if random() <= self.edata_fraction: # cache as evaluation data
-            self.edata['inputs'].append([x])
+            self.edata['inputs'].append(list(xs))
             self.edata['outputs'].append(y)
         else:
-            self.tdata['inputs'].append([x])
+            self.tdata['inputs'].append(list(xs))
             self.tdata['outputs'].append(y)
     
     def add_model(self, model):
@@ -77,6 +78,8 @@ class ModelBox(): # main MB class
         return self.test(idx)
 
     def test(self, idx):
+        if self.edata['inputs'] == []:
+            return False
         score = self.models[idx].score(self.edata['inputs'], self.edata['outputs'])
         res = score >= self.pmaccuracy
         print(f"Compare Model {idx} score = {score} with pmaccuracy {self.pmaccuracy} => production = {res}")
@@ -104,10 +107,12 @@ class ModelBox(): # main MB class
         self.state = 'embedded' 
 
     def add_dataset(self, x, y, kind='tdata'):
+        assert kind in ['tdata', 'edata'], "dataset kind must be 'tdata' or 'edata'"
         eval('self.'+ kind)['inputs'] += x
         eval('self.'+ kind)['outputs'] += y
 
     def remove_dataset(self, kind='tdata'):
+        assert kind in ['tdata', 'edata'], "dataset kind must be 'tdata' or 'edata'"
         eval('self.'+ kind)['inputs'] = []
         eval('self.'+ kind)['outputs'] = []
 
@@ -117,9 +122,9 @@ class ModelBox(): # main MB class
     def remove_function(self, idx):
         self.functions.pop(idx)
 
-    def get_feedback(self, x, y):
+    def get_feedback(self, y, *xs):
         if random() <= self.feedback_fraction:
-            newy = input(f"{x=:.1f}, {y=:.1f}. To override y, type a new value (float) and return, otherwise just press return:")
+            newy = input(f"x={roundl(list(xs))}, {y=:.1f}. To override y, type a new value (float) and return, otherwise just press return:")
             if newy != '':  
                 return float(newy)
         return y 
@@ -128,21 +133,21 @@ class ModelBox(): # main MB class
         print(f"{tag}: state:{self.state}, #functions:{len(self.functions)}, #models:{len(self.models)}, production:{self.isproduction}, #tdata:{len(self.tdata['inputs'])}, #edata:{len(self.edata['inputs'])}; tinputs:{'...' if len(self.tdata['inputs']) > ntail  else ''}{roundl(self.tdata['inputs'][-ntail:])}; toutputs:{'...' if len(self.tdata['outputs']) > ntail else ''}{roundl(self.tdata['outputs'][-ntail:])}; einputs:{'...' if len(self.edata['inputs']) > ntail  else ''}{roundl(self.edata['inputs'][-ntail:])}; eoutputs:{'...' if len(self.edata['outputs']) > ntail else ''}{roundl(self.edata['outputs'][-ntail:])})")
 
 def generate_data(f, count=1000):
-    return [[x] for x in range(count)], [f(x) for x in range(count)]
+    return [[x, x+1] for x in range(count)], [f(x, x+1) for x in range(count)]
 
 def repeat_function(f, count=10, scale=100):
     for _ in range(count):
-        f(random()*scale)
+        f(random()*scale, random()*scale)
 
 def roundl(xl, places=0):
-    if type(xl) == list: 
+    if type(xl) in (list, tuple): 
         return [roundl(x, places) for x in xl]
     return round(xl, places)
 
 mb = ModelBox()
 @mb.function_wrapper()
-def f(x): 
-    return 3 * x + 2
+def f(x0, x1): 
+    return 3 * x0 + x1
 
 mb.print('Intial MB')
 repeat_function(f)
@@ -164,13 +169,14 @@ mb.print('After training and testing the added model')
 repeat_function(f)
 mb.print('After a few more function calls')
 
-xy = generate_data(f)
-mb.add_dataset(xy[0], xy[1])
-mb.print('After adding more data but before training')
-mb.train_all()
-mb.print('After training and testing with the new data')
-repeat_function(f)
-mb.print('After a few more function calls')
+if mb.state == 'embedded':
+    xy = generate_data(f)
+    mb.add_dataset(xy[0], xy[1])
+    mb.print('After adding more data but before training')
+    mb.train_all()
+    mb.print('After training and testing with the new data')
+    repeat_function(f)
+    mb.print('After a few more function calls')
 
 if mb.isproduction[1] == False:
     mb.pmaccuracy = -100
@@ -196,15 +202,15 @@ repeat_function(f)
 mb.print('After a few more function calls')
 
 @mb.sensor_wrapper()
-def fcopy(x):
-    return 3 * x + 2
+def fcopy(x0, x1):  # y
+    return 3 * x0 + x1
 
 repeat_function(fcopy)
 mb.print('After a few direct-sensor calls')
 
 @mb.sensor_wrapper('inverse')
-def finv(y):
-    return y / 3 - 2
+def finv(y, x0):  # x1
+    return y - 3 * x0
 
 repeat_function(finv)
 mb.print('After a few inverse-sensor calls')
